efc899c4d0c246ba59402c3b4c3d0a84698b8278
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-06-28 08:34:48 -0700

    tested server, content type, body, and response

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const server = express();
 const configureMiddleware = require('./middleware/config-mw');
-const games = require('./routers/games-router')
+// const games = require('./routers/games-router');
 
 configureMiddleware(server);
 
 server.get('/', (req, res) => {
-    res.send(`<h2>HELLO THIS IS PATRICK</h2>`)
+    res.status(200).json({ message: 'HELLO THIS IS PATRICK' });
   });  
 
 
-server.use('/api/games', games);
+// server.use('/api/games', games);
 
 module.exports = server;
